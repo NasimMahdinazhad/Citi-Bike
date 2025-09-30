bf30bb4aa11dcb7c8c1b86fdad2f70e8a7681978
Nasim Mahdinazhad <nasimmahdinazhad@gmail.com>
2025-09-30 15:29:04 +0200

    Update final_dashboard.py

--- final_dashboard.py
+++ final_dashboard.py
@@ -121,7 +121,7 @@ elif page == 'Interactive map with aggregated bike trips':
     path_to_html = "Bike_Trips_NYC.html" 
 
     # Read file and keep in variable
-    with open(path_to_html,'r') as f: 
+    with open(path_to_html,'r', encoding="utf-8") as f: 
         html_data = f.read()
 
     ## Show in webpage
